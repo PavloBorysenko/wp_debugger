77d0ad1d502b524ef0e10f249d6d9cc809d1afa0
Pavlo Borysenko <irod-09@mail.ru>
2023-08-26 19:19:51 +0200

    init

---
+++ wp_debugger.php
@@ -0,0 +1,16 @@
+/*
+Plugin Name: Debugger for Wordpress
+Plugin URI: https://#
+Description: Assistant for finding errors and optimizing scripts
+Version: 0.0.1
+Requires at least: 5.8
+Requires PHP: 7.2
+Author: PavloBorysenko
+License: GPLv2 or later
+Text Domain: wp_debugger
+*/
+
+define('WPDEBUG_PATH', plugin_dir_path(__FILE__));
+define('WPDEBUG_LINK', plugin_dir_url(__FILE__));
+define('WPDEBUG_PLUGIN_NAME', plugin_basename(__FILE__));
+define('WPDEBUG_LOG_PATH', WPDEBUG_PATH . 'data/log/');
